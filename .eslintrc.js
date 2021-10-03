03b0964e8ed7a7a0ac1e4aa792f14fd695e5ee33
ilker0 <ilker46580@gmail.com>
2021-10-03 16:00:48 +0300

    eslint & prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended'],
+  rules: {
+    'vue/require-default-prop': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off'
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', '**prettier**']
+}
